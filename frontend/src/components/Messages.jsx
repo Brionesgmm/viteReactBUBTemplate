9f3b81b3f4ae14439d4ba61c34fe1a6f84fa3472
unknown <jbrionesgmm@gmail.com>
2023-09-18 12:31:37 -0600

    added closing for the error messages

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,26 +1,43 @@
+import { useState } from "react";
+
+const Message = ({ type, children }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  if (!isVisible) return null;
+
+  return (
+    <div className={"alert alert-dismissible fade show alert-" + type}>
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={() => setIsVisible(false)}
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
 const Messages = ({ messages }) => (
   <>
-    {messages.errors
-      ? messages.errors.map((el, i) => (
-          <div key={i} className="alert alert-danger">
-            {el.msg}
-          </div>
-        ))
-      : null}
-      {messages.success
-      ? messages.success.map((el, i) => (
-          <div key={i} className="alert alert-success">
-            {el.msg}
-          </div>
-        ))
-      : null}
-    {messages.info
-      ? messages.info.map((el, i) => (
-          <div key={i} className="alert alert-info">
-            {el.msg}
-          </div>
-        ))
-      : null}
+    {messages.errors &&
+      messages.errors.map((el, i) => (
+        <Message key={`${el.msg}-${i}`} type="danger">
+          {el.msg}
+        </Message>
+      ))}
+    {messages.success &&
+      messages.success.map((el, i) => (
+        <Message key={`${el.msg}-${i}`} type="success">
+          {el.msg}
+        </Message>
+      ))}
+    {messages.info &&
+      messages.info.map((el, i) => (
+        <Message key={`${el.msg}-${i}`} type="info">
+          {el.msg}
+        </Message>
+      ))}
   </>
 );
 
